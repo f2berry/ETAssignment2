5b6285c50bfc2a81ee544acca912719b624ab3a8
f2berry <arkan2b@gmail.com>
2013-03-09 18:54:10 +0000

    Ajout des controllers de page + 3 fonctions pour chaque page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def paintballField
+  end
+
+  def paintballMarker
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 WikiRails::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/paintballField"
+
+  get "pages/paintballMarker"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
