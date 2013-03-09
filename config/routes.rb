c889aa400bdfebee8ad5c0443a3a1e0c14f14e68
f2berry <arkan2b@gmail.com>
2013-03-09 20:24:45 +0000

    Add of correct routes to match the adress with /. Set of the home page to /pages/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 WikiRails::Application.routes.draw do
+
+  root :to => 'pages#home'
+
+  match '/home', :to => 'pages#home'
+  match '/paintballField',   :to => 'pages#paintballField'
+  match '/paintballMarker',    :to => 'pages#paintballMarker'
+
   get "pages/home"
-
   get "pages/paintballField"
-
   get "pages/paintballMarker"
+
+
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,10 +60,6 @@ WikiRails::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
